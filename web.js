2ca934b359265b1649d0cede3d00db4e4f544a87
woying <loveson821@gmail.com>
2013-11-02 21:50:55 +0800

    first commit

--- web.js
+++ web.js
@@ -1,12 +1,55 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+var express = require('express'),
+    path = require('path'),
+    http = require('http'),
+    fs = require('fs');
+var request = require('request');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var app = express();
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.all('/*',function(req,res,next){
+    res.setHeader('content-type','text/json; charset=UTF-8');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next(); 
+});
+
+app.get('/:id', function(req,res){
+  console.log(req.params.id)
+  request('http://www.xiami.com/song/gethqsong/sid/'+req.params.id, function (error, response, body) {
+    res.send( JSON.parse(response.body).location ) 
+  });
+});
+
+// app.get('/wines', wine.findAll);
+// app.get('/wines/:id', wine.findById);
+// app.post('/wines', wine.addWine);
+// app.put('/wines/:id', wine.updateWine);
+// app.delete('/wines/:id', wine.deleteWine);
+
+// app.post('/file-upload', function(req, res) {
+//     // get the temporary location of the file
+//     console.log(req.files);
+//     var tmp_path = req.files.file.path;
+//     // set where the file should actually exists - in this case it is in the "images" directory
+//     var target_path = './public/pics/'+req.files.file.name;
+//     // move the file from the temporary location to the intended location
+//     fs.rename(tmp_path, target_path, function(err) {
+//         if (err) throw err;
+//         // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+//         fs.unlink(tmp_path, function() {
+//             if (err) throw err;
+//             console.log('File uploaded to: ' + target_path + ' - ' + req.files.file.size + ' bytes');
+//             res.send(req.files.file.name);
+//         });
+//     });
+//   });
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
 });
